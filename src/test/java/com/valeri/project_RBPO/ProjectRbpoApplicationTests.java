80eb4881cfbc8ef02e071086864bd68ff039dfbf
valeri0-0 <inspacequeen@gmail.com>
2025-12-18 15:01:18 +0300

    Задание 6: HTTPS и CI

--- src/test/java/com/valeri/project_RBPO/ProjectRbpoApplicationTests.java
+++ src/test/java/com/valeri/project_RBPO/ProjectRbpoApplicationTests.java
@@ -2,12 +2,13 @@ package com.valeri.project_RBPO;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class ProjectRbpoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
